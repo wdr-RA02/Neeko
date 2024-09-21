8513c4b203bfc61f6f65f6ee2fbe6e226ed5d986
wdr-RA02 <cydia1829@gmail.com>
2024-09-21 04:03:18 +0000

    add data cleaning logic to shuffle_data

--- shuffle_data.py
+++ shuffle_data.py
@@ -2,6 +2,19 @@ import os
 import glob
 import json
 import random
+import re
+
+def filter_conv(dial, eot_token:str, role):
+    pat = re.compile(r"^(.*?)( \(.*?\): )")
+    dialogs = [d for d in dial.split(eot_token) if len(d)>0]
+
+    while len(dialogs)>0:
+        pat_match = re.findall(pat, dialogs[-1])
+        if len(pat_match)>0 and pat_match[0][0] == role:
+            break
+        _ = dialogs.pop()
+    
+    return eot_token.join([d for d in dialogs if len(re.findall(pat, d))>0]) + eot_token
 
 
 def main(
@@ -17,9 +30,15 @@ def main(
             role = jsonl_file.split("prompted_agent_dialogue_")[-1].replace('.jsonl', '')
             for line in file:
                 one = json.loads(line)
+                # 将没有当前role的条目删除掉
+                dial = one["output"].replace("\n", "")
                 one["role"] = role
                 one["eot"] = "<|eot|>"
-                data.append(one)
+                one["output"] = filter_conv(dial, one["eot"], role)
+                # 只有大于一轮的才予以append
+                conv = one["output"].removesuffix(one["eot"]).split(one["eot"])
+                if len(conv) > 1:
+                    data.append(one)
 
     random.shuffle(data)
 
